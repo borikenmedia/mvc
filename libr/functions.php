3e73e37d296f61cd4c45fea6cd64077ff16ff53d
Billy Laureano Centeno <37109363+borikenmedia@users.noreply.github.com>
2019-06-02 23:14:30 -0400

    Create functions.php

---
+++ libr/functions.php
@@ -0,0 +1,13 @@
+<?php
+
+/* Minthread CMS v2 Release Under LGPL3 By dyewilliam */
+
+function shtml($data){}
+
+function diag($data){}
+
+function ssid(){}
+
+function feeddata($sql, $type, $value){}
+
+?>
