a390491f44ccbff3948b4b16703dd2c915c24c6c
Billy Laureano Centeno <37109363+borikenmedia@users.noreply.github.com>
2020-01-05 01:46:26 -0400

    Create pages.class.php

---
+++ modules/models/pages.class.php
@@ -0,0 +1,13 @@
+<?php
+namespace modules\models;
+defined("APPPATH") or exit("Access denied");
+
+/* Minthread CMS v2 Release Under LGPL3 By dyewilliam */
+
+use libr\basemodel,
+    libr\basecontroller,
+    modules\interfaces\pages as admin;
+    
+class pages extends basecontroller implements admin{}
+
+?>
