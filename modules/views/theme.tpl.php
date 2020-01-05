7bf7593e8f2ebd49c8d9ef86f146d444ef934ae4
Billy Laureano Centeno <37109363+borikenmedia@users.noreply.github.com>
2020-01-05 01:40:32 -0400

    Create theme.tpl.php

---
+++ modules/views/theme.tpl.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html Public>
+<html lang="en-us">
+<head>
+<title>{title}</title>
+<meta name="description" content="Website Template" />
+<meta name="keywords" content="Web, Design, Development" />
+<meta name="aucthor" content="dyewilliam" />
+<meta charset="utf-8" />
+<script language="javascript" type="text/javascript">
+<!--
+
+//-->
+</script>
+<style type="text/css">
+<!--
+
+//-->
+</style>
+</head>
+<body>
+
+
+
+</body>
+</html>
