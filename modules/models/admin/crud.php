3e547ff9d85f40671eb45dd8616b303acb802ec3
Billy Laureano Centeno <37109363+borikenmedia@users.noreply.github.com>
2020-01-05 01:31:40 -0400

    Create crud.php

---
+++ modules/models/admin/crud.php
@@ -0,0 +1,13 @@
+<?php
+namespace modules\models\admin;
+defined("APPPATH") or exit("Access denied");
+
+use \libr\basecontroller,
+    \libr\basemodel,
+    \models\interfaces\crud as admin;
+
+/* Minthread CMS v2 Release Under LGPL3 By dyewilliam */
+
+class admin extends basecontroller implements admin{}
+
+?>
