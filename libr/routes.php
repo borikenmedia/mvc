b269db3ac12c4fbc465781627757abe0f4bc824d
Billy Laureano Centeno <37109363+borikenmedia@users.noreply.github.com>
2021-01-17 15:49:17 -0400

    Update routes.php

--- libr/routes.php
+++ libr/routes.php
@@ -2,6 +2,8 @@
 namespace libr;
 defined("Access denied") or exit("<n style=\"font: normal 1.65em Calibri;\">Access denied</n>");
 
+/* Mtd CMS v2 Release Under LGPL3 By dyewilliam (nickhere) */
+
 final class routes{
 
   private $_method = "pages";
