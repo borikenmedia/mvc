86c0cced459f7af63690801621726a490c5e8233
Billy Laureano Centeno <37109363+borikenmedia@users.noreply.github.com>
2021-01-17 16:27:53 -0400

    Update pagescrud.class.php

--- modules/interfaces/pagescrud.class.php
+++ modules/interfaces/pagescrud.class.php
@@ -1,1 +1,14 @@
+<?php
+namespace modules\interfaces;
+defined("APPATH") or exit("<n style=\"font: normal 1.65em Calibri;\">Access denied</n>");
 
+/* Mtd CMS v2 Release Under LGPL3 By dyewilliam (nickhere) */
+
+interface pagescrud{
+  public function createpage();
+  public function readpage();
+  public function updatepage();
+  public function droppage();
+}
+
+?>
