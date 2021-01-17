240d1a66db8b8ff87dfc0b9a5466964bbaeab8d0
Billy Laureano Centeno <37109363+borikenmedia@users.noreply.github.com>
2021-01-17 16:31:49 -0400

    Update postscrud.class.php

--- modules/interfaces/postscrud.class.php
+++ modules/interfaces/postscrud.class.php
@@ -1,1 +1,14 @@
+<?php
+namespace modules\interfaces;
+defined("APPATH") or exit("<n style=\"font: normal 1.65em Calibri;\">Access denied</n>");
 
+/* Mtd CMS v2 Release Under LGPL3 By dyewilliam */
+
+interface postscrud{
+  public function createpost();
+  public function readpost();
+  public function updatepost();
+  public function droppost();
+}
+
+?>
