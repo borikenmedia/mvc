25b0584c005ff2b0f5eeed978ef8237e371edd40
Billy Laureano Centeno <37109363+borikenmedia@users.noreply.github.com>
2021-01-16 23:04:36 -0400

    Update theme.tpl.php

--- modules/views/theme.tpl.php
+++ modules/views/theme.tpl.php
@@ -14,12 +14,113 @@
 <style type="text/css">
 <!--
 
+  *{MARGIN:0;PADDINMG:0;}
+  
+  Body{
+    margin: 0;
+    padding: 0;
+    background: #fff;
+    font: normal 1em Calibri, Arial, Verdana;
+    color: #000;
+  }
+  
+  div#container{
+    margin: 1em Auto;
+    width: 65%;
+    background: #f1f1f1;
+    border: solid 1px #ccc;
+  }
+  
+  div#logo{
+    margin: 0;
+    padding: 1%;
+    width: 98%;
+    background: #eee;
+  }
+  
+  div#tabmenu{
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    background: #ddd;
+  }
+  
+  div#tabmenu ul li{
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+  
+  div#tabmenu a{
+    margin: 0;
+    padding: 1%;
+    width: 23%;
+    float: left;
+    display: inline-table;
+  }
+  
+  div#sitemap{
+    margin: 0;
+    padding: 1%;
+    width: 98%;
+    background: #eee;
+  }
+  
+  div#infowrapp{}
+  
+  div#sidebar{
+    margin: 0;
+    padding: 1%;
+    width: 28%;
+    float: left;
+    display: inline-table;
+  }
+  
+  div#content{
+    margin: 0;
+    padding: 1%;
+    width: 68%;
+    background: #fff;
+    float: left;
+    display: inline-table;
+  }
+  
+  div#copyright{
+    margin: 0;
+    padding: 1%;
+    width: 98%;
+    background: #ddd;
+  }
+  
+  div#clear{clear:both;}
 //-->
 </style>
 </head>
 <body>
+  
+<div id="container">
+  <div id="logo">{logo}</div>
+  <div id="tabmenu">
+    <ul>
+      <li><a href="#" target="_self" title="LINK">HOME</a></li>
+      <li><a href="#" target="_self" title="LINK">ABOUT</a></li>
+      <li><a href="#" target="_self" title="LINK">SERVICE</a></li>
+      <li><a href="#" target="_self" title="LINK">CONTACT</a></li>
+    </ul>
+  </div>
+  <div id="clear"></div>
+  <div id="sitemap">HOME &minus;&gt; {sitemap}</div>
+  <div id="infowrapp">
+    <div id="sidebar">{sidebar}</div>
+    <div id="content">{content}</div>
+  </div>
+  <div id="clear"></div>
+  <div id="copyright">Copyright &copy; 2021 www.domain.com &minus; Terms &amp; Policy &minus; Contact</div>
+</div>
 
+<!--
   <h3>Under Construction</h3>
+//-->
 
 </body>
 </html>
