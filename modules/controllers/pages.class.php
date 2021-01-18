eb111f8c5d4597a7846d1513c962e909ea468665
Billy Laureano Centeno <37109363+borikenmedia@users.noreply.github.com>
2021-01-17 22:22:13 -0400

    Update pages.class.php

--- modules/controllers/pages.class.php
+++ modules/controllers/pages.class.php
@@ -1,1 +1,36 @@
+<?php
+namespace modules\controllers;
+defined("APPATH") or exit("<n style=\"font: normal 1.65em Calibri;\">Access denied</n>");
 
+/* Mtd CMS v2 Release Under LGPL3 By dyewilliam (nickhere) */
+
+use \libr\template as tpl;
+
+final class pages{
+
+  private $db;
+  private $tpl;
+  
+  public function __construct(){}
+  
+  public function home(){
+    $this->tpl = new tpl;
+    $values = array(
+    "title" => "Boriken Media Subs",
+    "urlreq" => URLREQ,
+    "sitemap" => "Welcome_TO_THE_HOME_PAGE",
+    "sidebar" => "SIDEBAR_MENU",
+    "content" => "CONTENT_BODY");
+    $render = $this->tpl->parsetpl($values)->output();
+    echo($render);
+  }
+  
+  public function about(){}
+  
+  public function profile(){}
+  
+  public function contact(){}
+
+}
+
+?>
