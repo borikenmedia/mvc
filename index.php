7536a572d3b8b1b662e4a8b044cf5f761114e22f
Billy Laureano Centeno <37109363+borikenmedia@users.noreply.github.com>
2021-01-25 09:56:41 -0400

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ ini_set("error_reporting", E_ALL);
 date_default_timezone_set("America/Puerto_Rico");
 header("Content-type: text/html; Charset: UTF-8; Pragma: no-cache;");
 
-/* Mtd CMS v2 Release Under LGPL3 By dyewilliam (nick) */
+/* Mtd CMS v2 Release Under LGPL3 By dyewilliam (nickhere) */
 
 define("PATH", __DIR__);
 define("APPATH", PATH."/modules/");
@@ -15,7 +15,7 @@ define("TIME", date("D M d, Y h:ia"));
 
 function autoclass($class){
   $path = PATH."/".str_replace("\\", "/", $class).".class.php";
-  (file_exists($path))? require_once($path): new \Exception("CORE_OBJECT:: Class::{$class} is not available", 1);
+  (file_exists($path))? require_once($path): new \Exception("CORE_OBJECT:: Class ({$class}) is not available", 1);
 }
 
 spl_autoload_register("autoclass");
