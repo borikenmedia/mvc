3101a2d27df73d1feafb096c1cb744069f23bb3d
Billy Laureano Centeno <37109363+borikenmedia@users.noreply.github.com>
2021-01-28 15:11:12 -0400

    Update pages.form.php

--- modules/assets/forms/pages.form.php
+++ modules/assets/forms/pages.form.php
@@ -1,1 +1,12 @@
 <!--Here_lies_the_pages_forms_file_it_witholds_as_many_as Needed//-->
+<form method="post" action="#" name="neewsletter">
+  Username or email<br/>
+  <input type="text" name="uname" value="user" /><br/>
+  Category<br/>
+  <select name="ncat">
+    <options value="null" selected>Choose Below</options>
+    <options value="technology">Technology Ezine</options>
+    <options value="programming">Computer Dev</options>
+  </select>
+  <input type="submit" name="send" value="Submit" /></form>
+<!--Newsletter_Subscribe_Form//-->
