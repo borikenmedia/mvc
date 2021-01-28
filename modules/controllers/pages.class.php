01dfdb622ef318beffb97b2b1733d06dd178f986
Billy Laureano Centeno <37109363+borikenmedia@users.noreply.github.com>
2021-01-28 14:55:24 -0400

    Update pages.class.php

--- modules/controllers/pages.class.php
+++ modules/controllers/pages.class.php
@@ -4,7 +4,7 @@ defined("APPATH") or exit("<n style=\"font: normal 1.65em Calibri;\">Access deni
 
 /* Mtd CMS v2 Release Under LGPL3 By dyewilliam (nickhere) */
 
-use \libr\template as tpl;
+use \libr\template as tpl, \modules\models\pages as db;
 
 final class pages{
 
@@ -31,6 +31,23 @@ final class pages{
   public function profile(){}
   
   public function contact(){}
+  
+  public function cpanel(){
+    $this->db = new db;
+    $this->tpl = new tpl;
+    $values = array(
+    "title" = "Boriken Media Subs",
+    "urlreq" => URLREQ,
+    "logo" => "<h1>Boriken Media Subs</h1>",
+    "sitemap" => "Welcome &minus;&gt; Cpanel",
+    "sidebar" => "Cpanel_Sidebar",
+    "content" => "Content &minus;&gt; Forms<br/>".$this->db->getforms("pages"));
+    $render = $this->tpl->parsetpl($values)->output();
+    $this->db->setLogs();
+    $this->db->state = $render;
+    $this->db->setCache();
+    echo($render);
+  }
 
 }
 
